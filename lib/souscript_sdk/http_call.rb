248d3f9723bd0f1360b4097e268edd50637112ac
Guillaume BOUDON <guillaumeboudon@gmail.com>
2021-10-13 15:05:35 +0200

    :recycle: Refactor code

--- lib/souscript_sdk/http_call.rb
+++ lib/souscript_sdk/http_call.rb
@@ -6,22 +6,23 @@ class SouscriptSDK
     def query(method:, args:, http_method: :get)
       res =
         case http_method.to_sym
-          when :get
-            uri = URI.parse(make_uri(method, args))
-            Net::HTTP
-              .get(uri)
-              .force_encoding("ISO-8859-1").encode("UTF-8") # Fix encoding issue with accentuated letters
-          when :post
-            Net::HTTP
-              .post_form(URI.parse(@base_url), post_params(method, args))
-              .body
-              .force_encoding("ISO-8859-1").encode("UTF-8")
-          else
-            raise "http method not handled : #{http_method}"
+        when :get
+          Net::HTTP.get(URI.parse(make_uri(args, base_params(method))))
+        when :post
+          Net::HTTP
+            .post_form(
+              URI.parse(@base_url),
+              post_params(args, base_params(method))
+            ).body
+        else
+          raise "HTTP method not handled : #{http_method}"
         end
+
       underscore_hash_keys(
         Hash.from_xml(
           res
+            .force_encoding("ISO-8859-1")
+            .encode("UTF-8")
             .gsub(/<(?=[^<>]*<)/, "&lt;")
             .gsub(/&(?!(?:amp|lt|gt|quot|apos);)/, "&amp;")
         )
@@ -30,10 +31,17 @@ class SouscriptSDK
 
     private
 
-    def make_uri(method, args)
+    def base_params(method)
+      {
+        ID: @user_name,
+        PWD: @password,
+        IDREQ: method
+      }
+    end
+
+    def make_uri(args, params)
       uri = @base_url
-      uri += "?ID=#{@user_name}&PWD=#{@password}"
-      uri += "&IDREQ=#{method}"
+      uri += "?#{params.map { |k, v| "#{k}=#{v}" }.join("&")}"
 
       args.each do |key, val|
         uri += "&#{key.upcase}=#{URI.encode_www_form_component(val)}"
@@ -42,8 +50,8 @@ class SouscriptSDK
       uri
     end
 
-    def post_params(method, args)
-      args.inject({'ID' => @user_name, 'PWD' => @password, 'IDREQ' => method}) do |memo, (key, val)|
+    def post_params(args, params)
+      args.each_with_object(params) do |(key, val), memo|
         memo[key.upcase] = URI.encode_www_form_component(val)
         memo
       end
